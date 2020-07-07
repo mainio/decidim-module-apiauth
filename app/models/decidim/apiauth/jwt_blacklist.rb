c20a50550e30ec8a5127778f127f3a7c05f7728a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-07-07 15:04:15 +0300

    Apply devise-jwt updates to the JwtBlacklist model

--- app/models/decidim/apiauth/jwt_blacklist.rb
+++ app/models/decidim/apiauth/jwt_blacklist.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Apiauth
     class JwtBlacklist < ApplicationRecord
-      include ::Devise::JWT::RevocationStrategies::Blacklist
+      include ::Devise::JWT::RevocationStrategies::Denylist
 
       self.table_name = "decidim_apiauth_jwt_blacklists"
     end
