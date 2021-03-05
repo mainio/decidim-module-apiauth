ba8e4c0fdbc803dd0f4794ba345ab86f336271bf
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-03-05 19:08:08 +0200

    Add secret generator for dummy app

--- Rakefile
+++ Rakefile
@@ -3,7 +3,11 @@
 require "decidim/dev/common_rake"
 
 desc "Generates a dummy app for testing"
-task test_app: "decidim:generate_external_test_app"
+task test_app: "decidim:generate_external_test_app" do
+  Dir.chdir("spec/decidim_dummy_app") do
+    system("bundle exec rails generate decidim:apiauth:install")
+  end
+end
 
 desc "Generates a development app."
 task development_app: "decidim:generate_external_development_app"
---
+++ lib/generators/decidim/apiauth/install_generator.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails/generators/base"
+
+module Decidim
+  module Apiauth
+    module Generators
+      class InstallGenerator < Rails::Generators::Base
+
+        def enable_authentication
+          secret = SecureRandom.hex(64)
+          secrets_path = Rails.application.root.join("config", "secrets.yml")
+
+          index = nil
+          i = 0
+          lines = IO.readlines(secrets_path).map do |line|
+            index = i if line =~ /^test:/
+            i += 1
+            line
+          end
+          unless lines[index + 3].include?("secret_key_jwt")
+            lines.insert(index + 3, "  secret_key_jwt: #{secret}")
+            File.open(secrets_path, "w") do |file|
+              file.puts lines
+            end
+          end
+        end
+      end
+    end
+  end
+end
